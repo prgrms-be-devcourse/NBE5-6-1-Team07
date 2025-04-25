ad3bb9bb69af9ad5c382760505a5812e87995999
pia01190 <piakim0119@gmail.com>
2025-04-25 19:26:34 +0900

    ##11 FIX: 인덱스 컨트롤러 수정

--- src/main/java/com/grepp/team07/IndexController.java
+++ src/main/java/com/grepp/team07/IndexController.java
@@ -1,14 +1,26 @@
 package com.grepp.team07;
 
+import com.grepp.team07.app.model.product.dto.ProductDto;
+import com.grepp.team07.app.model.product.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/")
 public class IndexController {
+
+    private final ProductService productService;
+
     @GetMapping
-    private String index(){
+    private String index(Model model){
+        List<ProductDto> products = productService.bestProduct();
+        model.addAttribute("products", products);
         return "index";
     }
 }
