c454d4fe49c8d058807dd14a0bff5ae2a6cdfa46
pia01190 <piakim0119@gmail.com>
2025-04-25 19:10:53 +0900

    FIX: 인덱스 컨트롤러 추가

---
+++ src/main/java/com/grepp/team07/IndexController.java
@@ -0,0 +1,14 @@
+package com.grepp.team07;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    private String index(){
+        return "index";
+    }
+}
